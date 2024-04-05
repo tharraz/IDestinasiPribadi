7043374586cf789837e440aee167455e2510c8a6
tharraz <156740230+tharraz@users.noreply.github.com>
2024-04-05 14:09:46 +0700

    Add files via upload

---
+++ Registrasi3.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Register Page</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://icons.getbootstrap.com/icons/check-circle-fill/"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="css/registrasi.css" />
+  </head>
+  <body>
+    <i class="bi bi-check-circle-fill"></i>
+    </div>
+    <div class="wrapper">
+      <form action="">
+        <center>
+        <h2>Yay! Akunmu Sudah Terdaftar</h2>
+        <p>Mulai perjalananmu dalam wisata Indonesia dengan Idestinasi</p>
+        </center>
+          
+      
+        <button type="submit" class="btn">Kembali ke Beranda</button>
+
+        
+    </div>
+  </body>
+</html>
